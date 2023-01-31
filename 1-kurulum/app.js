48d2d00e4ab6cc168fcab509f361113f35514e39
ichbinnahid <nahid.zarbiyev@gmail.com>
2023-02-01 02:33:26 +0400

    Data Annotation

--- 1-kurulum/app.js
+++ 1-kurulum/app.js
@@ -1,3 +1,10 @@
-console.log("hello world");
-console.log("test");
-console.log("1.ders typescript");
+// console.log("hello world");
+// console.log("test");
+// console.log("1.ders typescript");
+var age = 35;
+var firstName = "Nahid";
+// firstName = 35 // yanlis assign
+var isUpdated = true;
+function display(id, name) {
+    console.log(" id = " + id + ", Name = " + name);
+}
--- 1-kurulum/app.ts
+++ 1-kurulum/app.ts
@@ -1,4 +1,14 @@
-console.log("hello world");
-console.log("test");
-console.log("1.ders typescript");
+// console.log("hello world");
+// console.log("test");
+// console.log("1.ders typescript");
 
+let age: number = 35;
+let firstName: string = "Nahid";
+// firstName = 35 // yanlis assign
+
+let isUpdated: boolean = true;
+
+function display(id:any, name:string) {
+    console.log(" id = " + id + ", Name = " + name);
+    
+}
