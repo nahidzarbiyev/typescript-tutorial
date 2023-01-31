98a40eba2ef2b5e707215238f0048103b84a3de9
ichbinnahid <nahid.zarbiyev@gmail.com>
2023-02-01 02:46:54 +0400

    3. Data type Number String Boolean

--- 1-kurulum/app.js
+++ 1-kurulum/app.js
@@ -1,10 +1,31 @@
 // console.log("hello world");
 // console.log("test");
 // console.log("1.ders typescript");
-var age = 35;
-var firstName = "Nahid";
-// firstName = 35 // yanlis assign
-var isUpdated = true;
-function display(id, name) {
-    console.log(" id = " + id + ", Name = " + name);
-}
+// let age: number = 35;
+// let firstName: string = "Nahid";
+// // firstName = 35 // yanlis assign
+// let isUpdated: boolean = true;
+// function display(id:any, name:string) {
+//     console.log(" id = " + id + ", Name = " + name);
+// // }
+// let first: number = 123; //number
+// let second: number = 0x37cf; // hexadecimal
+// let third: number = 0o377; //octal
+// let fourth: number = 0b111001; // binary
+// console.log(first); // 123
+// console.log(second); //14287
+// console.log(third); //255
+// console.log(fourth); //57
+// let employeefirstName: string = 'Nahid Zerbiyev';
+// console.log(employeefirstName);
+// // let employeefirstName: string = "Nahid Zerbiyev";
+// let employeeName: string = 'nahid'
+// let employeeDept: string = 'finance'
+// //pre ES6
+// let employeeDesc1: string = employeeName + " works in the " + employeeDept + " department."
+// console.log(employeeDesc1);
+// //post ES6
+// let employeeDesc2:string = `${employeeName} works in the ${employeeDept} department`
+// console.log(employeeDesc2);
+var isPresent = true;
+console.log(isPresent);
--- 1-kurulum/app.ts
+++ 1-kurulum/app.ts
@@ -2,13 +2,51 @@
 // console.log("test");
 // console.log("1.ders typescript");
 
-let age: number = 35;
-let firstName: string = "Nahid";
-// firstName = 35 // yanlis assign
+// let age: number = 35;
+// let firstName: string = "Nahid";
+// // firstName = 35 // yanlis assign
 
-let isUpdated: boolean = true;
+// let isUpdated: boolean = true;
 
-function display(id:any, name:string) {
-    console.log(" id = " + id + ", Name = " + name);
-    
-}
+// function display(id:any, name:string) {
+//     console.log(" id = " + id + ", Name = " + name);
+
+// // }
+
+// let first: number = 123; //number
+
+// let second: number = 0x37cf; // hexadecimal
+
+// let third: number = 0o377; //octal
+
+// let fourth: number = 0b111001; // binary
+
+// console.log(first); // 123
+// console.log(second); //14287
+// console.log(third); //255
+// console.log(fourth); //57
+
+// let employeefirstName: string = 'Nahid Zerbiyev';
+// console.log(employeefirstName);
+
+// // let employeefirstName: string = "Nahid Zerbiyev";
+
+
+// let employeeName: string = 'nahid'
+// let employeeDept: string = 'finance'
+
+// //pre ES6
+
+// let employeeDesc1: string = employeeName + " works in the " + employeeDept + " department."
+
+// console.log(employeeDesc1);
+
+// //post ES6
+
+// let employeeDesc2:string = `${employeeName} works in the ${employeeDept} department`
+
+// console.log(employeeDesc2);
+
+
+let isPresent: boolean = true
+console.log(isPresent);
